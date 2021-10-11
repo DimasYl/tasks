5f0b24a6b69d0f716810dabb2aaff05cb0eed821
DimasYl <dima.ilyushin.2016@mail.ru>
2021-10-11 22:24:32 +0300

    fix && radio

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import HW7 from "../../h7/HW7";
+import style from './Junior.module.css'
 
 const Junior:React.FC = () => (
-    <div>
-        Junior
+    <div className={style.block}>
+        <HW7/>
     </div>
 );
 
